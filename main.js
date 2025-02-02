a4f63b92158a8fb5ded84fbc6d37400c725c7682
Ruthiemy K. Oribello <eh202201050@wmsu.edu.ph>
2025-02-03 01:58:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ coffeeProducts.forEach( (coffee)=>{
     div.classList.add('max-w-[300px]','h-fit', 'flex', 'flex-col', 'items-center', 'gap-2','cursor-pointer','overflow-hidden');
     div.id = coffee.id;
     const img = document.createElement('img');
-    img.classList.add('h-[70%]', 'hover:scale-105', 'ease-in','duration-200');
+    img.classList.add('max-h-[300px]', 'hover:scale-105', 'ease-in','duration-200');
     img.src = `./src/assets/product_image/${coffee.imageUrl}`;
     div.appendChild(img);
 
@@ -43,7 +43,7 @@ cookieProducts.forEach( (cookie)=>{
     div.classList.add('max-w-[300px]','h-fit', 'flex', 'flex-col', 'items-center', 'gap-2','cursor-pointer','overflow-hidden');
     div.id = cookie.id;
     const img = document.createElement('img');
-    img.classList.add('h-[70%]', 'hover:scale-105', 'ease-in','duration-200');
+    img.classList.add('max-h-[300px]', 'hover:scale-105', 'ease-in','duration-200');
     img.src = `./src/assets/product_image/${cookie.imageUrl}`;
     div.appendChild(img);
 
