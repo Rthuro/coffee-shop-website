38138ec9073c3c1b7c27c3f3e23a7db12a639b20
riyu.iring <oribelloruthiemy@gmail.com>
2025-02-04 18:08:02 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,3 @@
-export function head(title){
-    
-    const head = document.querySelector('head');
-
-    head.innerHTML = `
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Azure Waves Café | ${title}</title>
-    <link rel="shortcut icon" href="/src/assets/logo/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="/tailwind.css"> `;
-}
 
 export function header(page){
     const header = document.querySelector('header');
