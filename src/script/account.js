8bb330152c9cfee869de8b2f0440ae2abb43c599
riyu.iring <oribelloruthiemy@gmail.com>
2025-02-07 17:55:26 +0800

    Update account.js

--- src/script/account.js
+++ src/script/account.js
@@ -13,8 +13,29 @@ import { header, footer } from "/src/components/layout.js";
             accEmail.innerHTML = localStorage.getItem("currentUserEmail");
 
             logOut.addEventListener('click', ()=>{
+                // Set the currentUserEmail and currentUserPass to null
                 localStorage.setItem('currentUserEmail',null);
                 localStorage.setItem('currentUserPass',null);
+                window.location.href = "/index.html";
+            });
+
+            deleteAcc.addEventListener('click',()=>{
+             
+                const users = JSON.parse(localStorage.getItem('users'));
+                const userCart = JSON.parse(localStorage.getItem('userCart'));
+                
+                // Exclude the user from users 
+                const delUser = users.filter(user => user.email !== currentUser);
+                localStorage.setItem('users', JSON.stringify(delUser));
+
+                // Exclude the user cart item from userCart 
+                const removeDelUserCart = userCart.filter( item => item.userEmail !== currentUser);
+                localStorage.setItem('userCart', JSON.stringify(removeDelUserCart));
+
+                // Set the currentUserEmail and currentUserPass to null
+                localStorage.setItem('currentUserEmail',null);
+                localStorage.setItem('currentUserPass',null);
+
                 window.location.href = "/index.html";
             });
         }
